8b2242e645adef79ec5a248c45c7ba8b1578a5bc
Cannon Moyer <cannon.moyer@treadmilldoctor.com>
2018-04-18 21:48:06 +0000

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,5 @@
 
 
 
-Address.create(street: "1250 McIngvale Rd", state: "MS", city: "Hernando", zip: "38632", square_feet: 1000)
-Address.create(street: "9296 Brooks Drive", state: "MS", city: "Olive Branch", zip: "38654", square_feet: 10000)
-Address.create(street: "1234 Time Square", state: "NY", city: "New York City", zip: "77372", square_feet: 9500)
 
 User.create(email: "cannonmoyer@yahoo.com", password: "ljo183xs4", password_confirmation: "ljo183xs4")
