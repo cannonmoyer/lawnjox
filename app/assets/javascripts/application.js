6d32d99f5cd5b2d44d5c4b8c2b7e75f297e3c039
Cannon Moyer <cannon.moyer@treadmilldoctor.com>
2018-06-06 23:19:06 +0000

    fixed js error

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,61 +16,6 @@
 //= require turbolinks
 //= require_tree .
 
-$(document).ready(function() {
-    $("#address").keyup(function() {
-       $.ajax({
-            url: "/addresses/search",
-            crossDomain: true,
-            dataType: "JSONP",
-            data: { 
-                q: $("#address").val()
-            },
-            
-            type: "GET",
-            success: function(response) {
-        
-            },
-            error: function(xhr) {
-        
-            }
-        });
-     
-    });
-});
-    //$(".nav-list li a").click(function(e) {
-      //  e.preventDefault();
-      //$(this).effect( "shake" );
-    //});
-    
-    $(document).on("click", "#mowing", function(){
-      if(document.getElementById('mowing').checked) {
-            document.getElementById("lawn-mowing-qty").value = "1";
-        } else {
-            document.getElementById("lawn-mowing-qty").value = "0";
-        }
-    });
-    
-    $(document).on("click", "#spraying", function(){
-      if(document.getElementById('spraying').checked) {
-            document.getElementById("lawn-spraying-qty").value = "1";
-        } else {
-            document.getElementById("lawn-spraying-qty").value = "0";
-        }
-    });
-    
-    jQuery(document).on("click", "#results li", function(){
-        
-        var element = document.getElementById("address_id");
-        if(this.dataset.id != null){
-        element.value = this.dataset.id;
-        }
-        var address_textf = document.getElementById("address");
-        if(this.dataset.address != null){
-        address_textf.value = this.dataset.address;
-        }
-        jQuery("#results").html("");
-    });
-});
 
 
        
